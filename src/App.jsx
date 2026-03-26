512121c3bfa928879f97e006a80e46cc35d21ac0
Glamnart <favournchess192@gmail.com>
2026-03-26 18:19:04 +0100

    Creating all necessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+*/
 
-function App() {
-  return (
-    <div className="p-10 flex gap-4">
-      {/* Basic usage */}
-      <FontAwesomeIcon icon={faHouse} className="text-blue-500 size-6" />
-      
-      {/* With Tailwind classes for styling */}
-      <FontAwesomeIcon 
-        icon={faMagnifyingGlass} 
-        className="text-gray-400 hover:text-black cursor-pointer transition-colors" 
-      />
-    </div>
-  )
-
+export default function App(){
+  return <>todo</>
 }
-
-export default App
