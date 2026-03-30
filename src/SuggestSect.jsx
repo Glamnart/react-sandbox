3a37bd1d8a1e01463cdf1e339435a3f64a4789e6
Glamnart <favournchess192@gmail.com>
2026-03-30 22:43:06 +0100

    Adds Middle section

--- src/SuggestSect.jsx
+++ src/SuggestSect.jsx
@@ -1,8 +1,12 @@
+import { deal, cityHotels, citiesInfo, hotelReview } from "./neededInfo.js"
+
 const Title = ({h1,h2,m}) => {
     return <div flexName="flex flex-col items-center">
         <p className="text-center font-bold text-2xl text-gray-700 my-2">{h1}</p>
+        <div className="relative mb-15">
         <p className="text-center text-gray-600">{h2}</p>
-        <div className={`bg-sky-400 pt-1 w-20 my-5 ${"ml-" + m}`}></div>
+        <div className={`bg-sky-400 pt-1 w-25 absolute -bottom-5 ${m}`}></div>
+        </div>
     </div>
 }
 
@@ -17,26 +21,172 @@ const Destination = ({city, num, link})=>{
 }
 
 const Destinations = ()=>{
+
+    const destinationRow = (cityHotels)=>{
+        const row = Array(6).fill(null);
+
+        const destinations = row.map((city,cityNo)=>{
+            const {place,num,link} = cityHotels[cityNo];
+            return <Destination city={place} num={num} link={link}/>
+        })
+        return <div className="flex flex-row justify-center gap-5"> 
+         {destinations}
+        </div>
+    }
+
     return<div className="m-10">
         <Title 
         h1="Suggested Destinations in Nigeria" 
         h2="Below are the most popular travel destinations in Nigeria"
-        m={125}/>
-        <div className="flex flex-row justify-center gap-5"> 
-            <Destination city="Lagos" num="3,642" link="lagos"/>
-            <Destination city="Abuja" num="1,290" link="abuja"/>
-            <Destination city="Port Harcourt" num="449" link="rivers/port-harcourt"/>
-            <Destination city="Kaduna" num="331" link="kaduna"/>
-            <Destination city="Uyo" num="223" link="akwaibom/uyo"/>
-            <Destination city="Ibadan" num="518" link="oyo/ibadan"/>
+        m="left-123"/>
+        {destinationRow(cityHotels)}
+    </div>
+}
+
+const Deal = ({link,image,name,city,state,deal,cityLink,stateLink})=>{
+    return <div>
+        <div className="flex flex-col relative">
+            <a className="group" href={link}>
+                <img className="w-85 relative h-50" src={image} alt={name}/>
+                <div className="absolute top-0 bg-black/23 w-85 h-50 hidden group-hover:flex"></div>
+            </a>
+            <div className="border border-gray-400 border-t-0 flex flex-col gap-2 pl-4 pt-6 pb-4 w-85 rounded-b-md">
+            <a className="text-sky-400 font-bold w-52 overflow-hidden text-nowrap text-ellipsis text-xl" href={link}>{name}</a>
+            <div>
+                <a className="text-gray-700" href={`https://hotels.ng/hotels-in-${cityLink}`}>{city},</a>
+                <a className="text-gray-700" href={`https://hotels.ng/hotels-in-${stateLink}`}>{state}</a>
+            </div>
+            </div>
+            <div className="absolute top-3 -left-4">
+                <div className="text-white bg-orange-400 z-9 text-nowrap overflow-hidden text-sm py-1 px-3 flex items-center w-45">
+                    Up to <div className="text-xl font-bold mx-1">{deal}%</div> off
+                </div>
+                <div className="bg-orange-600 absolute left-1.5 -z-1 -bottom-3 w-5.5 rotate-45 py-3"> 
+                </div>
+            </div>
         </div>
     </div>
 }
 
+const Deals = ()=>{
+
+    const printDeals = (deal) => {
+        let table = Array(2).fill(Array(3).fill(null));
+
+        let rows = table.map((row,rowNo)=>{
+            const index = rowNo*3;
+
+            let tags = row.map((box, boxNo)=>{
+                const {link,image,name,city,state,dealNo,cityLink,stateLink} = deal[index+boxNo]
+                return <Deal link = {link} image={image} name={name} city={city} state={state} deal={dealNo} cityLink={cityLink} stateLink={stateLink}/>
+            })
+
+            return <div className="flex gap-7.5 my-8">
+                {tags}
+            </div>
+        }) 
+        return rows;
+    }
+
+
+    return <div>
+        <Title 
+        h1="Today's Top Hotel Deals"
+        h2="A selection of the best hotel deals, only available today"
+        m="left-123"/>
+        {printDeals(deal)}
+    </div>
+}
+
+const Popular = ({place,no,link,bgPic}) =>{
+    return <div className="relative group overflow-hidden">
+        <div className={`${bgPic} bg-black/40 bg-blend-overlay bg-cover w-80 h-60 rounded-md text-white flex flex-col items-center bg-no-repeat bg-center justify-center`}>
+            <p className="text-xl">{no}</p>
+            <p className="text-2xl font-bold">{place + " Hotels"}</p>
+        </div>
+        
+        <div className="bg-black/50 h-20 w-80 items-center justify-center absolute bottom-0 group-hover:translate-y-0 flex ease-in-out transform transition-transform duration-300 origin-bottom translate-y-full">
+            <a href={`https://hotels.ng/hotels-in-${link}`} className="border-white border text-white p-3 rounded-md hover:bg-sky-400 hover:border-0">
+                <button className="cursor-pointer">{"Book Hotels in " + place}</button>
+            </a>
+        </div>
+    </div>
+}
+
+const Populars = ()=>{
+
+    const printCities = (citiesInfo) => {
+        let table = Array(2).fill(Array(3).fill(null));
+
+        let rows = table.map((row,rowNo)=>{
+            const index = rowNo*3;
+
+            let tags = row.map((box, boxNo)=>{
+                const {place, no, link, bgPic} = citiesInfo[index+boxNo]
+                return <Popular place={place} no={no} link={link} bgPic={bgPic}/>
+            })
+
+            return <div className="flex gap-7.5 my-8">
+                {tags}
+            </div>
+        }) 
+        return rows;
+    }
+
+    return<div>
+        <Title 
+        h1="Popular cities with Hotels.ng travellers"
+        h2="See the top destinations people are traveling to"
+        m="left-115"/>
+        {printCities(citiesInfo)}
+    </div>
+}
+
+const City = ({city,numHotel,numReview})=>{
+    return <div className="flex flex-col text-wrap w-40 items-start"  >
+        <p className="font-bold text-sky-400 ">{`Hotels in ${city}`}</p>
+        <div className="text-sm text-gray-700">{`${numHotel} hotels in ${city} with over ${numReview} hotel reviews`}</div>
+    </div>
+}
+
+const Cities = () => {
+
+    const printList = (hotelReview) => {
+        let table = Array(8).fill(Array(5).fill(null));
+
+        let rows = table.map((row,rowNo)=>{
+            const index = rowNo*5;
+
+            let places = row.map((place, placeNo)=>{
+                const {city,numHotel,numReview} = hotelReview[index+placeNo]
+                if((index+placeNo) > 37)
+                    return  <></>
+                else     
+                    return <City city={city} numHotel={numHotel} numReview={numReview}/>
+            })
+
+            return <div className="flex gap-x-8 my-4">
+                {places}
+            </div>
+        }) 
+        return rows;
+    }
+
+    return<div>
+        <Title 
+        h1="Which City Do You Want To Book A Hotel?"
+        h2="13,728 hotels in 1483 cities/ towns in Nigeria"
+        m="left-106"/>
+        {printList(hotelReview)}
+    </div>
+}
 
 const SuggestSect=()=>{
     return <div className="flex flex-col items-center justify-around m-20">
         <Destinations/>
+        <Deals/>
+        <Populars/>
+        <Cities/>
     </div>
 }
 
---
+++ src/neededInfo.js
@@ -0,0 +1,123 @@
+export const deal = [
+    {
+        link:"https://hotels.ng/hotel/HN350983S-florence-hotel-and-suites?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=florence-hotel-and-suites",
+        image:"/assets/hotel1.jpeg",
+        name:"Florence Hotel and Suites",
+        city:"Benin",
+        state:"Edo",
+        dealNo:20,
+        cityLink:"edo/benin",
+        stateLink:"edo"
+    },
+    {
+        link:"https://hotels.ng/hotel/1391154-corinthia-villa-hotel-suites?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=the-corinthia-villa-h-otel-suites",
+        image:"/assets/hotel2.jpeg",
+        name:"The Corinthia Villa Hotel & Suites",
+        city:"Garki 2",
+        state:"Abuja",
+        dealNo:27,
+        cityLink:"abuja/garki-2",
+        stateLink:"abuja"
+    },
+    {
+        link:"https://hotels.ng/hotel/1470842-luxol-homes-suites?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=luxol-homes-suites",
+        image:"/assets/hotel3.jpeg",
+        name:"Luxol Homes & Suites",
+        city:"Ikeja",
+        state:"Lagos",
+        dealNo:21,
+        cityLink:"lagos/ikeja",
+        stateLink:"lagos"
+    },
+    {
+        link:"https://hotels.ng/hotel/26241-ibis-lagos-airport-lagos?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=delta-continental-hotel-suites",
+        image:"/assets/hotel4.jpeg",
+        name:"Delta Continental Hotel & Suites",
+        city:"Ajao estate",
+        state:"Lagos",
+        dealNo:19,
+        cityLink:"lagos/ajao-estate",
+        stateLink:"lagos"
+    },
+    {
+        link:"https://hotels.ng/hotel/1470786-bon-hotel-garden-city-port-harcourt?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=white-stone-hotel-and-resort-limited",
+        image:"/assets/hotel5.jpeg",
+        name:"White Stone Hotel and Resort Limited",
+        city:"Port harcourt",
+        state:"Rivers",
+        dealNo:19,
+        cityLink:"rivers/port-harcourt",
+        stateLink:"rivers"
+    },
+    {
+        link:"https://hotels.ng/hotel/1470633-wavecrest-beach-resort?utm_source=home&utm_campaign=top-deals&utm_medium=on-site&utm_content=wavecrest-beach-resort",
+        image:"/assets/hotel6.jpeg",
+        name:"Wavecrest Beach Resort",
+        city:"Ibeju-lekki",
+        state:"Lagos",
+        dealNo:0,
+        cityLink:"lagos/ibeju-lekki",
+        stateLink:"lagos"
+    }
+]
+
+export const cityHotels =[
+    {place:"Lagos", num:"3,642", link:"lagos"},
+    {place:"Abuja", num:"1,290", link:"abuja"},
+    {place:"Port Harcourt", num:"449", link:"rivers/port-harcourt"},
+    {place:"Kaduna", num:"331", link:"kaduna"},
+    {place:"Uyo", num:"223", link:"akwaibom/uyo"},
+    {place:"Ibadan", num:"518", link:"oyo/ibadan"}
+]
+
+export const citiesInfo =[
+    {place:"Lagos", no:"3,642", link:"lagos", bgPic:"bg-[url(/assets/city1.jpeg)]"},
+    {place:"Abuja", no:"1,290", link:"abuja", bgPic:"bg-[url(/assets/city2.jpeg)]"},
+    {place:"Calabar", no:"289", link:"crossriver/calabar", bgPic:"bg-[url(/assets/city3.jpeg)]"},
+    {place:"Port Harcourt", no:"449", link:"rivers/port-harcourt", bgPic:"bg-[url(/assets/city4.jpeg)]"},
+    {place:"Owerri", no:"259", link:"imo/owerri", bgPic:"bg-[url(/assets/city5.jpeg)]"},
+    {place:"Uyo", no:"223", link:"akwaibom/uyo", bgPic:"bg-[url(/assets/city6.jpeg)]"}
+]
+
+export const hotelReview =[
+    {city:"Abia", numHotel:"298", numReview:"212"},
+    {city:"Abuja", numHotel:"1,290", numReview:"2,727"},
+    {city:"Adamawa", numHotel:"71", numReview:"58"},
+    {city:"Akwa Ibom", numHotel:"490", numReview:"370"},
+    {city:"Anambra", numHotel:"306", numReview:"192"},
+    {city:"Bauchi", numHotel:"51", numReview:"76"},
+    {city:"Bayelsa", numHotel:"182", numReview:"74"},
+    {city:"Benue", numHotel:"285", numReview:"66"},
+    {city:"Borno", numHotel:"24", numReview:"28"},
+    {city:"Cross River", numHotel:"361", numReview:"420"},
+    {city:"Delta", numHotel:"672", numReview:"531"},
+    {city:"Ebonyi", numHotel:"101", numReview:"28"},
+    {city:"Edo", numHotel:"403", numReview:"559"},
+    {city:"Ekiti", numHotel:"235", numReview:"220"},
+    {city:"Enugu", numHotel:"367", numReview:"519"},
+    {city:"Fct", numHotel:"1", numReview:"no"},
+    {city:"Gombe", numHotel:"43", numReview:"35"},
+    {city:"Imo", numHotel:"357", numReview:"387"},
+    {city:"Jigawa", numHotel:"35", numReview:"11"},
+    {city:"Kaduna", numHotel:"331", numReview:"302"},
+    {city:"Kano", numHotel:"129", numReview:"150"},
+    {city:"Katsina", numHotel:"33", numReview:"30"},
+    {city:"Kebbi", numHotel:"25", numReview:"15"},
+    {city:"Kogi", numHotel:"262", numReview:"65"},
+    {city:"Kwara", numHotel:"335", numReview:"319"},
+    {city:"Lagos", numHotel:"3,642", numReview:"12,664"},
+    {city:"Nasarawa", numHotel:"232", numReview:"80"},
+    {city:"Niger", numHotel:"110", numReview:"74"},
+    {city:"Ogun", numHotel:"766", numReview:"570"},
+    {city:"Ondo", numHotel:"259", numReview:"183"},
+    {city:"Osun", numHotel:"332", numReview:"221"},
+    {city:"Oyo", numHotel:"678", numReview:"819"},
+    {city:"Plateau", numHotel:"288", numReview:"169"},
+    {city:"Rivers", numHotel:"568", numReview:"743"},
+    {city:"Sokoto", numHotel:"22", numReview:"60"},
+    {city:"Taraba", numHotel:"98", numReview:"7"},
+    {city:"Yobe", numHotel:"11", numReview:"2"},
+    {city:"Zamfara", numHotel:"27", numReview:"12"},
+    {city:"", numHotel:"", numReview:""},
+    {city:"", numHotel:"", numReview:""}
+]
